536c68b22523a1b794eb78b9da220c3d3b635098
FernandezLinaresEsauAbimael <al222110763@gmail.com>
2024-06-29 01:18:26 -0600

    Creacion de un usuario, en la carpeta Models, ademas de definir las ruta

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
+const userRoutes = require("/Workspace/Materias/REACT-NATIVE/api_idgs_92/routes/user")
 
 const app = express();
 require('./config/databases')
 
-const userRoutes = require('./routes/user');
-app.use('/api/users', userRoutes);
+// Milddleware
+app.use(express.json());
+app.use("/api", userRoutes); 
+
+// Routes 
+app.get("/", (req, res) => {
+    res.send("Bienvenido a mi API")
+})
 
 const port = process.env.PORT || 3000;
 const uridb = process.env.MONGODB_URI || 'Hola esta parte si funciona'; 
